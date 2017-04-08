488f5a9becc5fdd282764cf76f2e23d73b176d67
Stan Wijckmans <stanw@aviovision.com>
2017-04-08 15:55:42 +0200

    1 Add new sounds + refactor.

--- app/src/main/java/be/stip/soundboard/IAppComponent.java
+++ app/src/main/java/be/stip/soundboard/IAppComponent.java
@@ -3,19 +3,14 @@ package be.stip.soundboard;
 import javax.inject.Singleton;
 
 import be.stip.soundboard.helpers.HelpersModule;
-import be.stip.soundboard.services.AssetService;
 import be.stip.soundboard.services.ServicesModule;
-import be.stip.soundboard.services.SoundPlayService;
-import be.stip.soundboard.viewmodels.ButtonsViewModel;
-import be.stip.soundboard.viewmodels.ViewModelsModule;
 import be.stip.soundboard.views.ButtonsView;
 import dagger.Component;
+import stannieman.mvvm.MvvmModule;
 
 @Singleton
-@Component(modules = {AppModule.class, ServicesModule.class, HelpersModule.class, ViewModelsModule.class})
+@Component(modules = {AppModule.class, ServicesModule.class, HelpersModule.class, MvvmModule.class})
 public interface IAppComponent {
-    void inject(AssetService assetService);
-    void inject(SoundPlayService soundPlayService);
-    void inject(ButtonsViewModel buttonsViewModel);
+    void inject(SplashScreenActivity splashScreenActivity);
     void inject(ButtonsView buttonsView);
 }
